50131642deca13044d8d56497b8f884c69f910c3
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-27 15:18:57 +1300

    Update test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.set_page_config(page_title="Price Scraper", layout="wide")
+
+def convert_data(data):
+    return data.to_csv(index=False).encode('utf-8')
+
+uploaded_file_0 = st.file_uploader("Upload a file", key="file_uploader_0")
+
+if uploaded_file_0 is not None and uploaded_file_1 is not None:
+    try:
+        pass
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+else:
+    st.info("Please upload a file to get started.")
