24b8701da3b0646bd2eddc7a932190f18d0b7748
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-27 15:26:18 +1300

    Update test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -5,12 +5,12 @@ st.set_page_config(page_title="Test chart", layout="wide")
 def convert_data(data):
     return data.to_csv(index=False).encode('utf-8')
 
-data = st.file_uploader("Upload a file", key="file_uploader_0")
+data = st.file_uploader("Upload a file", key="data")
 
 if data is not None:
     try:
         column_opt = [str(i) for i in data.columns]
-        column = st..multiselect("Dolumn", options = column_opt)
+        column = st..multiselect("Column", options = column_opt)
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
