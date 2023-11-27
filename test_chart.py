f60ad11cbc56f964c8dd2361c942d27afbfc466d
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-27 16:44:57 +1300

    Update test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -19,7 +19,7 @@ if uploaded_file is not None:
         # Create an interactive scatter plot with Plotly Express
         fig = px.scatter(data, x=x_axis, y=y_axis, title="Interactive Scatter Plot")
         st.plotly_chart(fig)
-        fig.update_layout(height=500, width=800)
+        fig.update_layout(height=800, width=800)
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
