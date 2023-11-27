062b70e27fda29da45c2acbe2c03f83287c1ad56
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-27 16:19:55 +1300

    Update test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -1,4 +1,4 @@
-import streamlit as st
+"""import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Test chart", layout="wide")
@@ -26,6 +26,34 @@ if uploaded_file_0 is not None:
             x_axis = st.selectbox("X-axis", columns)
             y_axis = st.selectbox("Y-axis", columns)
             st.scatter_chart(data, x=x_axis, y=y_axis)
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+else:
+    st.info("Please upload a file to get started.")"""
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title="Interactive Chart", layout="wide")
+
+uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
+
+if uploaded_file is not None:
+    try:
+        data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
+
+        st.dataframe(data)
+
+        column_options = [str(col) for col in data.columns]
+        x_axis = st.selectbox("Select X-axis:", options=column_options)
+        y_axis = st.selectbox("Select Y-axis:", options=column_options)
+
+        # Create an interactive scatter plot with Plotly Express
+        fig = px.scatter(data, x=x_axis, y=y_axis, title="Interactive Scatter Plot")
+        st.plotly_chart(fig)
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
