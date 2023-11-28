720f9c40d0467dad271dd6ce9f0f336d644450b4
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-28 13:15:47 +1300

    Update test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -9,23 +9,20 @@ uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 if uploaded_file is not None:
     try:
         data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
-        column_options = [str(col) for col in data.columns]
+        column_options = [""] + [str(col) for col in data.columns]
 
         ###Filters
+    
+        st.sidebar.header("Filters")
+        filter1 = st.sidebar.selectbox("Select filter:", options=column_options)
+        
+        if len(filter1)>0:
+            filter1_list = [str(i) for i in data[filter1].unique()]
+            filter1_list.sort()
+            filter1_selected = st.sidebar.multiselect(filter1, options=filter1_list)
 
-        filter_switch = st.sidebar.radio("Do you want a filter?", ("No", "Yes"))
-        while filter_switch == "Yes":
-    
-            st.sidebar.header("Filters")
-            filter1 = st.sidebar.selectbox("Select filter:", options=column_options)
-            
-            if len(filter1)>0:
-                filter1_list = [str(i) for i in data[filter1].unique()]
-                filter1_list.sort()
-                filter1_selected = st.sidebar.multiselect(filter1, options=filter1_list)
-    
-            if len(filter1_selected) == 0:
-                filter1_selected = filter1_list
+        if len(filter1_selected) == 0:
+            filter1_selected = filter1_list
         
         data = data[data[filter1].isin(filter1_selected)]
 
