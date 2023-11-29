c51e4dd8839e10ff09a0c88633d93d3c1220b03f
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-30 10:51:04 +1300

    Update Combine_code_and_text.py

--- Combine_code_and_text.py
+++ Combine_code_and_text.py
@@ -11,19 +11,21 @@ if uploaded_file is not None:
         headers = ['Code', 'Name']        
         data = pd.read_csv(uploaded_file, header=None, names=headers) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file, header=None, names=headers)
         
-        data['Code'] = [str(i).split('.')[0] for i in data['Code']]
+        data = data.dropna()
+
+        data['Code'] = [str(int(i)) for i in data['Code']]
         data['Name'] = [str(i) for i in data['Name']]
         
         ########################################
         ########################################
         def contains_number(s):
             return any(char.isdigit() for char in s)
-
+        
         i = 0
-        check = False
-        while check == False:
-            for cell in data['Code']:
-              check = contains_number(str(cell))
+        for cell in data['Code']:
+            if any(char.isdigit() for char in str(cell)):
+                break
+            else:
               i =+ 1
         
         data = data.iloc[i:,:]
@@ -33,7 +35,7 @@ if uploaded_file is not None:
         data['Combined'] = data.iloc[:,0] + " " + data.iloc[:,1]
         
         result_string = ', '.join(data['Combined'])
-        
+
         st.write(result_string)
     
     except Exception as e:
