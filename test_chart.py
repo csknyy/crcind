22c596898b2d099d2a8bdb0a9142e93dc98ba66f
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-30 11:21:23 +1300

    Update test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -8,12 +8,14 @@ uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 
 if uploaded_file is not None:
     try:
-        delimeter_check = st.sidebar.radio("Manual delimeter", ("No", "Yes"))
-        if delimeter_check == "Yes":
-            delimeter = st.text_input('Enter delimeter',keys='delimeter')
-            data = pd.read_csv(uploaded_file, encoding='ISO-8859-1', delimiter=delimeter) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file, encoding='ISO-8859-1')
-        else:
-            data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
+        #delimeter_check = st.sidebar.radio("Manual delimeter", ("No", "Yes"))
+        #if delimeter_check == "Yes":
+        #    delimeter = st.text_input('Enter delimeter',keys='delimeter')
+        #    data = pd.read_csv(uploaded_file, encoding='ISO-8859-1', delimiter=delimeter) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file, encoding='ISO-8859-1')
+        #else:
+        #    data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
+
+        pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
         column_options = [str(col) for col in data.columns]
 
         st.dataframe(data)
