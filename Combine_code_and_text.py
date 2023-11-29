4883c5d1e8387fd8f37c7f7f6d00b2e8f007638e
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-30 10:33:08 +1300

    Update Combine_code_and_text.py

--- Combine_code_and_text.py
+++ Combine_code_and_text.py
@@ -5,19 +5,23 @@ st.set_page_config(page_title="Interactive Chart", layout="wide")
 
 uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 
-if uploaded_file is not None:
+if uploaded_file:
     try:
-        data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
+        headers = ['Code', 'Name']
+        data = pd.read_csv(uploaded_file, header=None, names=headers) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file, header=None, names=headers)
         
-        data.iloc[:, 0] = data.iloc[:, 0].astype(str)
-        data.iloc[:, 1] = data.iloc[:, 1].astype(str)
-        
-        data['Combined'] = data.iloc[:,0] + " " + data.iloc[:,1]
-        
+        data.iloc[:, :2] = data.iloc[:, :2].astype(str)
+
+        i = 0
+        while not any(char.isdigit() for char in str(data.iloc[i, 0])):
+            i += 1
+
+        data = data.iloc[i:, :]
+        data['Combined'] = data.iloc[:, 0] + " " + data.iloc[:, 1]
+
         result_string = ', '.join(data['Combined'])
-        
         st.write(result_string)
-    
+
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
