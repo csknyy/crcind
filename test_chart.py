afd97b4f5aebfe58b54e8d7d6001eb4574b78bf3
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-30 11:19:54 +1300

    Update test_chart.py

--- test_chart.py
+++ test_chart.py
@@ -8,174 +8,166 @@ uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 
 if uploaded_file is not None:
     try:
-        delimiters = [',', ';', '\t']  # Add more delimiters if needed
-        data = None
-        
-        for delimiter in delimiters:
-            try:
-                data = pd.read_csv(uploaded_file, encoding='ISO-8859-1', delimiter=delimiter) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file, encoding='ISO-8859-1')
-                break  # Stop trying if successful
-            except pd.errors.ParserError:
-                pass  # Try the next delimiter if parsing fails
-        
-        if data is None:
-            st.error("Failed to parse the CSV file. Please check the file format.")
+        delimeter_check = st.sidebar.radio("Manual delimeter", ("No", "Yes"))
+        if delimeter_check = "Yes"
+            delimeter = st.text_input('Enter delimeter',keys='delimeter")
+            data = pd.read_csv(uploaded_file, encoding='ISO-8859-1', delimiter=delimeter) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file, encoding='ISO-8859-1')
         else:
+            data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
+        column_options = [str(col) for col in data.columns]
 
-            column_options = [str(col) for col in data.columns]
-    
-            st.dataframe(data)
-    
-            st.markdown('---')
-            st.header('Grouped data')
-    
-            ##############################
-            #####Filters
-            ##############################
-    
-            apply_filters = st.sidebar.radio("Apply filters", ("No", "Yes"))
-    
-            st.sidebar.header("Filters")
-            filter1 = st.sidebar.selectbox("Select filter:", options=column_options)
-            filter1_list = [str(i) for i in data[filter1].unique()]
-            filter1_list.sort()
-    
-            if apply_filters == "Yes":
-                if len(filter1)>0:
-                    filter1_selected = st.sidebar.multiselect(filter1, options=filter1_list)
-                    data = data[data[filter1] == filter1_selected]
-                    
-                if len(filter1_selected) == 0:
-                    filter1_selected = filter1_list
-    
-            ##############################
-            #####Groupby
-            ##############################
-            
-            try:
-                left_column, middle_column, right_column = st.columns(3)
-                with left_column:
-                    groupby_list = [str(i) for i in data.columns]
-                    groupby_selected = st.multiselect("Group data by", options = column_options)
-                with middle_column:
-                    groupby_data = data.groupby(by=groupby_selected).sum()
-                    selected_columns = st.multiselect("Selected columns", options = [str(col) for col in groupby_data.columns])
-                with right_column:
-                    top_group = st.text_input('"+" for first x rows and "-" for last x rows"', key="top_group")
-                    try:
-                        top_group = int(top_group)
-                    except:
-                        if not top_group:
-                            pass
-                        else:
-                            st.info("Please enter an integer")
-    
-                groupby_data = groupby_data[selected_columns].reset_index()
+        st.dataframe(data)
+
+        st.markdown('---')
+        st.header('Grouped data')
+
+        ##############################
+        #####Filters
+        ##############################
+
+        apply_filters = st.sidebar.radio("Apply filters", ("No", "Yes"))
+
+        st.sidebar.header("Filters")
+        filter1 = st.sidebar.selectbox("Select filter:", options=column_options)
+        filter1_list = [str(i) for i in data[filter1].unique()]
+        filter1_list.sort()
+
+        if apply_filters == "Yes":
+            if len(filter1)>0:
+                filter1_selected = st.sidebar.multiselect(filter1, options=filter1_list)
+                data = data[data[filter1] == filter1_selected]
                 
-                if not top_group:
-                    groupby_data_top = groupby_data.copy()
-                elif int(top_group) > 0:
-                    groupby_data_top = groupby_data.head(int(top_group))
-                elif int(top_group) < 0:
-                    groupby_data_top = groupby_data.tail(int(top_group)*-1)
-                else:
-                    pass
-                
-                st.dataframe(groupby_data_top)
+            if len(filter1_selected) == 0:
+                filter1_selected = filter1_list
+
+        ##############################
+        #####Groupby
+        ##############################
         
-                #groupby_list = [str(i) for i in data.columns]
-                #groupby_selected = st.multiselect("Group by", options = column_options)
-                #if len(groupby_selected) != 0:
-                #    groupby_data = data.groupby(by=groupby_selected).sum()
-                #    selected_columns = st.multiselect("Selected columns", options = [str(col) for col in groupby_data.columns])
-                #    groupby_data = groupby_data[selected_columns]
-                #    st.dataframe(groupby_data)
-            except:
-                pass
-            
-            ##############################
-    
-            st.markdown('---')
-            st.header('Chart')
-    
+        try:
             left_column, middle_column, right_column = st.columns(3)
             with left_column:
-                data_type = st.radio("", ("Raw data", "Grouped data"))
-    
-                if data_type == "Raw data":
-                    data = data
-                else:
-                    data = groupby_data
-                
+                groupby_list = [str(i) for i in data.columns]
+                groupby_selected = st.multiselect("Group data by", options = column_options)
             with middle_column:
-                chart_type = st.radio("", ("Scatter", "Scatter Matrix", "Line", "Bar"))
+                groupby_data = data.groupby(by=groupby_selected).sum()
+                selected_columns = st.multiselect("Selected columns", options = [str(col) for col in groupby_data.columns])
             with right_column:
-                top_chart = st.text_input('"+" for first x rows and "-" for last x rows"', key="top_chart")
+                top_group = st.text_input('"+" for first x rows and "-" for last x rows"', key="top_group")
                 try:
-                    top_chart = int(top_chart)
+                    top_group = int(top_group)
                 except:
-                    if not top_chart:
+                    if not top_group:
                         pass
                     else:
                         st.info("Please enter an integer")
-    
-            if not top_chart:
-                pass
-            elif int(top_chart) > 0:
-                data = data.head(int(top_chart))
+
+            groupby_data = groupby_data[selected_columns].reset_index()
+            
+            if not top_group:
+                groupby_data_top = groupby_data.copy()
+            elif int(top_group) > 0:
+                groupby_data_top = groupby_data.head(int(top_group))
             elif int(top_group) < 0:
-                data = data.tail(int(top_chart)*-1)
+                groupby_data_top = groupby_data.tail(int(top_group)*-1)
             else:
                 pass
             
-            new_column_options = [str(col) for col in data.columns]
+            st.dataframe(groupby_data_top)
     
-            if chart_type == "Scatter":
-                x_axis = st.selectbox("Select X-axis:", options=new_column_options)
-                y_axis = st.selectbox("Select Y-axis:", options=new_column_options)
-                fig = px.scatter(data, x=x_axis, y=y_axis)
-                
-            elif chart_type == "Bar":
-                x_axis = st.selectbox("Select X-axis:", options=new_column_options)
-                y_axis = st.selectbox("Select Y-axis:", options=new_column_options)
-                fig = px.bar(data, x=x_axis, y=y_axis)
-    
-            elif chart_type == "Line":
-                x_axis = st.selectbox("Select X-axis:", options=new_column_options)
-                y_axis = st.selectbox("Select Y-axis:", options=new_column_options)
-                fig = px.line(data, x=x_axis, y=y_axis)
-                
-            elif chart_type == "Scatter Matrix":
-                pd.DataFrame.iteritems = pd.DataFrame.items
-                numerical_columns = data.select_dtypes(include='number').columns.tolist()
-                all_numerical = st.radio("All numerical data", ("No","Yes"))
-                if all_numerical == "Yes":
-                    dimensions = st.multiselect("Dimensions", options = new_column_options, default = numerical_columns)
-                else:
-                    dimensions = st.multiselect("Dimensions", options = new_column_options)
-    
-                max_values = [max(data[dim])*1.1 for dim in dimensions]
-                min_values = [min(data[dim]) for dim in dimensions]
-                
-                label_name = st.text_input('Enter data label name', key="label_name")
-                
-                if label_name == "":
-                    fig = px.scatter_matrix(data, dimensions=dimensions)
-                else:
-                    try:
-                        fig = px.scatter_matrix(data, dimensions=dimensions, color = label_name)
-                    except:
-                        fig = px.scatter_matrix(data, dimensions=dimensions)
-    
-                for i, dim in enumerate(dimensions):
-                    fig.update_layout({"xaxis"+str(i+1): dict(range=[min_values[i], max_values[i]])})
-                    fig.update_layout({"yaxis"+str(i+1): dict(range=[min_values[i], max_values[i]])})
-                    
+            #groupby_list = [str(i) for i in data.columns]
+            #groupby_selected = st.multiselect("Group by", options = column_options)
+            #if len(groupby_selected) != 0:
+            #    groupby_data = data.groupby(by=groupby_selected).sum()
+            #    selected_columns = st.multiselect("Selected columns", options = [str(col) for col in groupby_data.columns])
+            #    groupby_data = groupby_data[selected_columns]
+            #    st.dataframe(groupby_data)
+        except:
+            pass
+        
+        ##############################
+
+        st.markdown('---')
+        st.header('Chart')
+
+        left_column, middle_column, right_column = st.columns(3)
+        with left_column:
+            data_type = st.radio("", ("Raw data", "Grouped data"))
+
+            if data_type == "Raw data":
+                data = data
             else:
-                pass
+                data = groupby_data
+            
+        with middle_column:
+            chart_type = st.radio("", ("Scatter", "Scatter Matrix", "Line", "Bar"))
+        with right_column:
+            top_chart = st.text_input('"+" for first x rows and "-" for last x rows"', key="top_chart")
+            try:
+                top_chart = int(top_chart)
+            except:
+                if not top_chart:
+                    pass
+                else:
+                    st.info("Please enter an integer")
+
+        if not top_chart:
+            pass
+        elif int(top_chart) > 0:
+            data = data.head(int(top_chart))
+        elif int(top_group) < 0:
+            data = data.tail(int(top_chart)*-1)
+        else:
+            pass
+        
+        new_column_options = [str(col) for col in data.columns]
+
+        if chart_type == "Scatter":
+            x_axis = st.selectbox("Select X-axis:", options=new_column_options)
+            y_axis = st.selectbox("Select Y-axis:", options=new_column_options)
+            fig = px.scatter(data, x=x_axis, y=y_axis)
+            
+        elif chart_type == "Bar":
+            x_axis = st.selectbox("Select X-axis:", options=new_column_options)
+            y_axis = st.selectbox("Select Y-axis:", options=new_column_options)
+            fig = px.bar(data, x=x_axis, y=y_axis)
+
+        elif chart_type == "Line":
+            x_axis = st.selectbox("Select X-axis:", options=new_column_options)
+            y_axis = st.selectbox("Select Y-axis:", options=new_column_options)
+            fig = px.line(data, x=x_axis, y=y_axis)
+            
+        elif chart_type == "Scatter Matrix":
+            pd.DataFrame.iteritems = pd.DataFrame.items
+            numerical_columns = data.select_dtypes(include='number').columns.tolist()
+            all_numerical = st.radio("All numerical data", ("Yes","No"))
+            if all_numerical == "Yes":
+                dimensions = st.multiselect("Dimensions", options = new_column_options, default = numerical_columns)
+            else:
+                dimensions = st.multiselect("Dimensions", options = new_column_options)
+
+            max_values = [max(data[dim])*1.1 for dim in dimensions]
+            min_values = [min(data[dim]) for dim in dimensions]
+            
+            label_name = st.text_input('Enter data label name', key="label_name")
+            
+            if label_name == "":
+                fig = px.scatter_matrix(data, dimensions=dimensions)
+            else:
+                try:
+                    fig = px.scatter_matrix(data, dimensions=dimensions, color = label_name)
+                except:
+                    fig = px.scatter_matrix(data, dimensions=dimensions)
+
+            for i, dim in enumerate(dimensions):
+                fig.update_layout({"xaxis"+str(i+1): dict(range=[min_values[i], max_values[i]])})
+                fig.update_layout({"yaxis"+str(i+1): dict(range=[min_values[i], max_values[i]])})
                 
-            fig.update_layout(height=800, width=1200, font=dict(size=8))
-            st.plotly_chart(fig)
+        else:
+            pass
+            
+        fig.update_layout(height=800, width=1200, font=dict(size=8))
+        st.plotly_chart(fig)
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
