2991f3b12f8f349cd374f594dd62b4a60a4df37a
csknyy <55134627+csknyy@users.noreply.github.com>
2023-11-30 08:45:25 +1300

    Update Combine_code_and_text.py

--- Combine_code_and_text.py
+++ Combine_code_and_text.py
@@ -7,14 +7,17 @@ uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 
 if uploaded_file is not None:
     try:
-      data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
-      
-      data['Combined'] = data.iloc[:,0] + data.iloc[:,1]
-      
-      result_string = ', '.join(data['Combined'])
-      
-      st.write(result_string)
-
+        data = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('csv') else pd.read_excel(uploaded_file)
+        
+        data.iloc[:, 0] = data.iloc[:, 0].astype(str)
+        data.iloc[:, 1] = data.iloc[:, 1].astype(str)
+        
+        data['Combined'] = data.iloc[:,0] + data.iloc[:,1]
+        
+        result_string = ', '.join(data['Combined'])
+        
+        st.write(result_string)
+    
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
