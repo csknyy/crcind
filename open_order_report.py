d87b2c1cf01c94ea6d64c004131770f235fac3c9
csknyy <55134627+csknyy@users.noreply.github.com>
2024-03-01 15:01:53 +1300

    Update open_order_report.py

--- open_order_report.py
+++ open_order_report.py
@@ -24,7 +24,7 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None:
         st.dataframe(data)
 
         csv = convert_data(data)
-        st.download_button(label="Download data as CSV", data=csv, file_name='Min_Max_with_supplier_request.csv', mime='text/csv')
+        st.download_button(label="Download data as CSV", data=csv, file_name='open_orders_report.csv', mime='text/csv')
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
