d8ae76b477e41e77a71987e938664a98f3224d4d
csknyy <55134627+csknyy@users.noreply.github.com>
2024-03-01 15:36:38 +1300

    Update open_order_report.py

--- open_order_report.py
+++ open_order_report.py
@@ -27,7 +27,7 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None:
 
         st.header(f'Total : ${int(data["Open Ordered $"].sum()*100)/100}')
         st.subheader(f'Total lines: {len(data["Open Ordered $"])}')
-        st.subheader(f'Total qty: {data["Open Qty"].sum()}').format(formatter="{,:.0f}")
+        st.subheader(f'Total qty: {data["Open Qty"].sum():,.0f}')
 
         st.dataframe(data.style.format(subset=["Open Ordered $"], formatter="${:,.2f}").format(subset=["Sales Order Number"], formatter="{:.0f}"))
 
