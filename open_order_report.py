fa74b72f44951742f32acb9d4aec21a06cbf7ba1
csknyy <55134627+csknyy@users.noreply.github.com>
2024-03-01 15:08:43 +1300

    Update open_order_report.py

--- open_order_report.py
+++ open_order_report.py
@@ -19,7 +19,9 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None:
 
         data = PowerBI.groupby(D3FO["Number"])[["Open Ordered $", "Open Qty"]].sum().sort_values(by="Open Ordered $", ascending=False)
         
-        data = data[data["Open Ordered $"] != 0].index.rename('Sales Order Number')
+        data = data[data["Open Ordered $"] != 0]
+
+        data.index.rename('Sales Order Number', inplace=True)
 
         st.dataframe(data)
 
