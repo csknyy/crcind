defe004ffd5afd91676ba709a31fe034861788f4
csknyy <55134627+csknyy@users.noreply.github.com>
2024-03-13 15:19:46 +1300

    Update open_order_report.py

--- open_order_report.py
+++ open_order_report.py
@@ -32,7 +32,7 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None:
         st.subheader(f'Total qty: {data["Open Qty"].sum():,.0f}')
 
         #st.dataframe(data.style.format(subset=["Open Ordered $"], formatter="${:,.2f}").format(subset=["Sales Order Number"], formatter="{:.0f}"))
-        st.dataframe(data.style.format(subset=["Open Ordered $"], formatter="${:,.2f}"))
+        st.dataframe(data.style.format(subset=["Open Ordered $"], formatter="${:,.2f}").format(subset=["Open Qty"], formatter="{:,.0f}")
 
         csv = convert_data(data)
         st.download_button(label="Download data as CSV", data=csv, file_name='open_orders_report.csv', mime='text/csv')
