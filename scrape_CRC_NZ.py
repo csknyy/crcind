3a94b2309c3dc2c463a1e020dc203a484adfe928
csknyy <55134627+csknyy@users.noreply.github.com>
2024-03-18 15:58:59 +1300

    Update scrape_CRC_NZ.py

--- scrape_CRC_NZ.py
+++ scrape_CRC_NZ.py
@@ -79,7 +79,7 @@ if len(crc_codes)>0:
             #feats_bens_list = soup.find_all('div', class_='product-feature-benefits').find('p').get_text(strip=True).replace(':', ': ').replace('.', '. ')
             #data_dict['Features/Benefits'] = feats_bens_list[1]
 
-            temp = response.text
+            temp = response.text.split("Features/Benefits: </span></h3><p>")[1].split("</p>")[0]
             
             st.write(temp)
             
