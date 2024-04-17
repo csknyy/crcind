cc6e6f6ad6230428e7b207ba6e79cdd48851f63c
csknyy <55134627+csknyy@users.noreply.github.com>
2024-04-17 13:34:06 +1200

    Create Mitre10_missing_items.py

---
+++ Mitre10_missing_items.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Mitre 10 missing items", layout="wide")
+
+uploaded_file = st.file_uploader("Upload a file")
+
+if uploaded_file is not None:
+  try:  
+    data = pd.read_excel(uploaded_file, sheet_name="RangeGaps", header = 1)
+    st.dataframe(data)
