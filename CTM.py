9227d4696569ea45779f5bc8e81f57dd596a41bf
csknyy <55134627+csknyy@users.noreply.github.com>
2024-05-16 10:46:35 +1200

    Update CTM.py

--- CTM.py
+++ CTM.py
@@ -24,7 +24,6 @@ if uploaded_file is not None:
 
         data_grouped1['CTM %'] = data_grouped1['CTM'] / total_CTM
         
-        
         st.dataframe(data_grouped1.style.format(subset=["Sales $"], formatter="${:,.2f}").format(subset=["GP $"], formatter="${:,.2f}").format(subset=["GP %"], formatter="%{:,.2f}"))
         st.dataframe(data)
     
