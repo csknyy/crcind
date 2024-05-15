e27796d28503b198b07b1b55b302e52cedcc3d34
csknyy <55134627+csknyy@users.noreply.github.com>
2024-05-16 10:51:55 +1200

    Update CTM.py

--- CTM.py
+++ CTM.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
         
         data_grouped1['GP %'] = data_grouped1['GP $'] / data_grouped1['Sales $']
 
-        data_grouped1['CTM'] = data_grouped1['CTS'] * data_grouped1['GP %']
+        data_grouped1['CTM'] = data_grouped1['CTS %'] * data_grouped1['GP %']
 
         total_CTM = data_grouped1['CTM'].sum()
 
