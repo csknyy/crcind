32a88e483f10a7afd6adf7017a4bcd267919bd17
csknyy <55134627+csknyy@users.noreply.github.com>
2024-05-16 10:50:20 +1200

    Update CTM.py

--- CTM.py
+++ CTM.py
@@ -24,7 +24,7 @@ if uploaded_file is not None:
 
         data_grouped1['CTM %'] = 100 * data_grouped1['CTM'] / total_CTM
         
-        st.dataframe(data_grouped1.style.format(subset=["Sales $"], formatter="${:,.2f}").format(subset=["GP $"], formatter="${:,.2f}").format(subset=["GP %"], formatter="%{:,.2f}").format(subset=["CTM %"], formatter="%{:,.4f}"))
+        st.dataframe(data_grouped1.style.format(subset=["Sales $"], formatter="${:,.2f}").format(subset=["GP $"], formatter="${:,.2f}").format(subset=["GP %"], formatter="%{:,.2f}").format(subset=["CTM %"], formatter="%{:,.2f}"))
         st.dataframe(data)
     
     except Exception as e:
