19bf55add0fe36433c364127827a1a59ff233ce7
csknyy <55134627+csknyy@users.noreply.github.com>
2024-05-16 12:26:53 +1200

    Update CTM.py

--- CTM.py
+++ CTM.py
@@ -66,7 +66,6 @@ elif report == "Mitre 10":
             data2 = data[['SubDepartment', 'FineLine', 'Item Description', 'Sales $','GP $']]
 
             for i in ['SubDepartment', 'FineLine', 'Item Description']:
-            #for i in ['Item Description']:
     
                 st.header(f"By {i}")
                 
@@ -123,6 +122,51 @@ else:
                 data = pd.read_excel(uploaded_file, header = int(header_ind))
             else:
                 data = pd.read_excel(uploaded_file)
+
+            column_names = data.columns
+
+            item_description = st.selectbox("Select item description", options = column_names)
+            sales$ = st.selectbox("Select Sales $", options = column_names)
+            GP$ =  st.selectbox("Select GP $", options = column_names)
+
+            groupby_columns = st.selectbox("Select GP $", options = column_names)
+
+            data2 = data[[item_description, sales$, GP$, groupby_columns]]
+
+            data2 = data2.rename(columns={item_description : 'Item Description'})
+            data2 = data2.rename(columns={sales$ : 'Sales $'})
+            data2 = data2.rename(columns={GP$ : 'GP $'})
+
+            for i in groupby_columns:
+            #for i in ['Item Description']:
+    
+                st.header(f"By {i}")
+                
+                data_grouped1 = data2.groupby(by=i).sum()[['Sales $','GP $']]
+                
+                total_sales = data_grouped1['Sales $'].sum()
+                data_grouped1['CTS %'] = 100 * data_grouped1['Sales $'] / total_sales
+                
+                data_grouped1['GP %'] = data_grouped1['GP $'] / data_grouped1['Sales $']
+        
+                data_grouped1['CTM'] = data_grouped1['CTS %'] * data_grouped1['GP %'] / 100
+        
+                total_CTM = data_grouped1['CTM'].sum()
+        
+                data_grouped1['CTM %'] = 100 * data_grouped1['CTM'] / total_CTM
+        
+                data_grouped1['Check'] = data_grouped1['CTM %'] - data_grouped1['CTS %']
+        
+                data_grouped1 = data_grouped1.drop(columns=['GP $', 'CTM'])
+                
+                st.dataframe(data_grouped1.style.format(subset=["Sales $"], formatter="${:,.2f}")
+                             .format(subset=["CTS %"], formatter="%{:,.2f}")
+                             .format(subset=["GP %"], formatter="%{:,.2f}")
+                             .format(subset=["CTM %"], formatter="%{:,.2f}")
+                             .format(subset=['Check'], formatter="%{:,.2f}")
+                            )
+    
+                st.markdown('---')
 
 
             st.dataframe(data)
