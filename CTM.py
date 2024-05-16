57e84b624e55ffd7579dfa7a54caf97cc7980f8c
csknyy <55134627+csknyy@users.noreply.github.com>
2024-05-16 13:25:58 +1200

    Update CTM.py

--- CTM.py
+++ CTM.py
@@ -37,8 +37,6 @@ if report == "Bunnings":
                 data_grouped1 = data_grouped1.drop(columns=['GP $', 'CTM'])
                 
                 st.dataframe(data_grouped1.style.format(subset=["Sales $"], formatter="${:,.2f}")
-                             .format(subset=["Units"], formatter="%{:,.0f}")
-                             .format(subset=["Avg Price"], formatter="%{:,.2f}")
                              .format(subset=["CTS %"], formatter="%{:,.2f}")
                              .format(subset=["GP %"], formatter="%{:,.2f}")
                              .format(subset=["CTM %"], formatter="%{:,.2f}")
@@ -93,6 +91,8 @@ elif report == "Mitre 10":
                 data_grouped1 = data_grouped1.drop(columns=['GP $', 'CTM'])
                 
                 st.dataframe(data_grouped1.style.format(subset=["Sales $"], formatter="${:,.2f}")
+                             .format(subset=["Units"], formatter="%{:,.0f}")
+                             .format(subset=["Avg Price"], formatter="%{:,.2f}")
                              .format(subset=["CTS %"], formatter="%{:,.2f}")
                              .format(subset=["GP %"], formatter="%{:,.2f}")
                              .format(subset=["CTM %"], formatter="%{:,.2f}")
