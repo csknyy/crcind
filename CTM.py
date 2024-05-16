6c9cef3e850742510f6a68d70c44a2a8dee70d6d
csknyy <55134627+csknyy@users.noreply.github.com>
2024-05-16 13:00:27 +1200

    Update CTM.py

--- CTM.py
+++ CTM.py
@@ -163,7 +163,7 @@ else:
                 total_sales = data_grouped1['Sales $'].sum()
                 data_grouped1['CTS %'] = 100 * data_grouped1['Sales $'] / total_sales
                 
-                data_grouped1['GP %'] = data_grouped1['GP $'] / data_grouped1['Sales $']
+                data_grouped1['GP %'] = 100 * data_grouped1['GP $'] / data_grouped1['Sales $']
         
                 data_grouped1['CTM'] = data_grouped1['CTS %'] * data_grouped1['GP %'] / 100
         
