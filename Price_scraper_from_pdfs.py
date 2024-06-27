9f5259d2855929cfeef95871c6681ffe78dec44c
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-27 15:24:21 +1200

    Update Price_scraper_from_pdfs.py

--- Price_scraper_from_pdfs.py
+++ Price_scraper_from_pdfs.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 uploaded_file = st.file_uploader("Choose your .pdf file", type="pdf")
 
-st.write(uploaded_file)
+pdf_data = open(uploaded_file, "rb").read()
+
+st.write(pdf_data)
