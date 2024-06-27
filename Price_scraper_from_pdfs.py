c0376006e181c30f3ddfeb1543522d2a0d1b5204
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-27 15:34:00 +1200

    Update Price_scraper_from_pdfs.py

--- Price_scraper_from_pdfs.py
+++ Price_scraper_from_pdfs.py
@@ -2,12 +2,17 @@ import streamlit as st
 import pdfplumber
 import pandas as pd
 
-uploaded_file = st.file_uploader("Choose your .pdf file", type="pdf", accept_multiple_files=True)
+# File uploader for multiple files
+uploaded_files = st.file_uploader("Choose your .pdf files", type="pdf", accept_multiple_files=True)
 
-with pdfplumber.open(uploaded_file) as pdf_file:
-  pdf_text = ""
-  for page in pdf_file.pages:
-      pdf_text += page.extract_text()
+# Check if files have been uploaded
+if uploaded_files:
+    all_text = ""
+    for uploaded_file in uploaded_files:
+        with pdfplumber.open(uploaded_file) as pdf_file:
+            pdf_text = ""
+            for page in pdf_file.pages:
+                pdf_text += page.extract_text()
+            all_text += pdf_text + "\n\n"  # Adding new lines for separation between different PDFs
 
-
-st.write(pdf_text)
+    st.text_area("Extracted Text", all_text, height=500)
