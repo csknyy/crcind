1e2e532b83d7bd7ce22335265d78937fd0fce1c4
csknyy <55134627+csknyy@users.noreply.github.com>
2024-08-27 10:14:09 +1200

    Update compare_price_files.py

--- compare_price_files.py
+++ compare_price_files.py
@@ -22,21 +22,38 @@ for uploaded_file,file in zip(uploaded_files,files):
     st.write("Uploaded:", uploaded_file.name)
     i = i+1
 
+for i in range(len(files)):
+    files[i].columns = files[i].columns.str.replace(' ', '_')
+
+data1 = files[0]
+data2 = files[1]
+
+customer_opt_0 = [str(i) for i in data2["Customer"].unique()]
+customer_opt_0.sort()
+customer_0 = st.multiselect("Customer",options = customer_opt_0,default=customer_opt_0)
+
+legacy_id_0 = [str(i) for i in data2["Legacy_Item_Number"].unique()]
+legacy_id_0.sort()
+legacy_id_0 = st.multiselect("Legacy_Item_Number",options = legacy_id_0,default=legacy_id_0)
+
+data1 = data1.query("Customer == @customer_0 & Legacy_Item_Number == @legacy_id_0")
+data2 = data2.query("Customer == @customer_0 & Legacy_Item_Number == @legacy_id_0")
+
 left_column0, right_column0 = st.columns(2)
 with left_column0:
-    st.dataframe(files[0])
+    st.dataframe(data1)
 with right_column0:
-    st.dataframe(files[1])
+    st.dataframe(data2)
     
 st.markdown('---')
         
 data1 = files[0]
 data2 = files[1]
 
-data11 = data1[['Date','Customer','Legacy Item Number','Item Name','Price']]
-data22 = data2[['Date','Customer','Legacy Item Number','Item Name','Price']]
+data11 = data1[['Date','Customer','Legacy_Item_Number','Item_Name','Price']]
+data22 = data2[['Date','Customer','Legacy_Item_Number','Item_Name','Price']]
 
-merged_data = pd.merge(data11, data22, on=['Customer', 'Legacy Item Number', 'Item Name'], suffixes=('_old', '_new'))
+merged_data = pd.merge(data11, data22, on=['Customer', 'Legacy_Item_Number', 'Item_Name'], suffixes=('_old', '_new'))
 price_increases = merged_data[merged_data['Price_new'] > merged_data['Price_old']]
 
 price_increases['Price_old'] = pd.to_numeric(price_increases['Price_old'], errors='coerce')
@@ -44,10 +61,10 @@ price_increases['Price_new'] = pd.to_numeric(price_increases['Price_new'], error
 
 price_increases.fillna(0, inplace=True)
 
-price_increases['Price change'] = price_increases['Price_new'] - price_increases['Price_old']
-price_increases['Price change %'] = (price_increases['Price change'] / price_increases['Price_old']) * 100
+price_increases['Price_change'] = price_increases['Price_new'] - price_increases['Price_old']
+price_increases['Price_change_%'] = (price_increases['Price_change'] / price_increases['Price_old']) * 100
 
-price_increases = price_increases[price_increases['Price change'] != 0]
+price_increases = price_increases[price_increases['Price_change'] != 0]
 
 customer_opt = [str(i) for i in price_increases["Customer"].unique()]
 customer_opt.sort()
