c5e5cf54dbbc2ae49d179ef3c8b4f8e686048de0
csknyy <55134627+csknyy@users.noreply.github.com>
2024-08-27 11:00:25 +1200

    Update compare_price_files.py

--- compare_price_files.py
+++ compare_price_files.py
@@ -68,8 +68,8 @@ data11 = data1[['Date','Customer','Legacy_Item_Number','Item_Name','Price']]
 data22 = data2[['Date','Customer','Legacy_Item_Number','Item_Name','Price']]
 
 merged_data = pd.merge(data11, data22, on=['Customer', 'Legacy_Item_Number', 'Item_Name'], suffixes=('_old', '_new'))
-merged_data['Price_old'] = pd.to_numeric(merged_data['Price_old'], errors='coerce')
-merged_data['Price_new'] = pd.to_numeric(merged_data['Price_new'], errors='coerce')
+#merged_data['Price_old'] = pd.to_numeric(merged_data['Price_old'], errors='coerce')
+#merged_data['Price_new'] = pd.to_numeric(merged_data['Price_new'], errors='coerce')
 #price_increases = merged_data[merged_data['Price_new'] > merged_data['Price_old']]
 #price_increases['Price_old'] = pd.to_numeric(price_increases['Price_old'], errors='coerce')
 #price_increases['Price_new'] = pd.to_numeric(price_increases['Price_new'], errors='coerce')
