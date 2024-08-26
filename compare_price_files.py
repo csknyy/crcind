9f66e35afbe9fde8ad4205630a5a2cb4d433db21
csknyy <55134627+csknyy@users.noreply.github.com>
2024-08-27 09:58:44 +1200

    Update compare_price_files.py

--- compare_price_files.py
+++ compare_price_files.py
@@ -18,9 +18,12 @@ for uploaded_file,file in zip(uploaded_files,files):
     st.write("Uploaded:", uploaded_file.name)
     i = i+1
 
-st.dataframe(files[0])
-st.dataframe(files[1])
-
+left_column0, right_column0 = st.columns(2)
+    with left_column0:
+        st.dataframe(files[0])
+    with middle_column0:
+        st.dataframe(files[1])
+        
 data1 = files[0]
 data2 = files[1]
 
