ada1b64cc9cb9c8b9b975243ba96b6ed7cdb9e78
csknyy <55134627+csknyy@users.noreply.github.com>
2024-10-09 12:37:39 +1300

    Update AU_Total_tools_report_diff.py

--- AU_Total_tools_report_diff.py
+++ AU_Total_tools_report_diff.py
@@ -32,12 +32,14 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None:
     except:
         pass
     
-    merged_data = pd.merge(current_month, prior_month_sliced, on=index, how='left')
+    merged_data = pd.merge(current_month, prior_month_sliced, left_index=True, right_index=True, how='left')
     st.dataframe(merged_data)
     
+    merged_data.iloc[:, 0] = pd.to_numeric(merged_data.iloc[:, 0], errors='coerce').fillna(0)
+    merged_data.iloc[:, 1] = pd.to_numeric(merged_data.iloc[:, 1], errors='coerce').fillna(0)
     merged_data.iloc[:, -2] = pd.to_numeric(merged_data.iloc[:, -2], errors='coerce').fillna(0)
     merged_data.iloc[:, -1] = pd.to_numeric(merged_data.iloc[:, -1], errors='coerce').fillna(0)
-    merged_data.iloc[:, 1] = pd.to_numeric(merged_data.iloc[:, 1], errors='coerce').fillna(0)
+    
     
     merged_data['Monthly Sales'] = merged_data.iloc[:, 0] - merged_data.iloc[:, -2]
     merged_data['Monthly Sales'] = merged_data['Monthly Sales'].replace(0, np.nan)
