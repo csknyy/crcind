c415f4f89486a78e235a4791843ccb8c89fa4154
csknyy <55134627+csknyy@users.noreply.github.com>
2024-10-09 11:59:20 +1300

    Update AU_Total_tools_report_diff.py

--- AU_Total_tools_report_diff.py
+++ AU_Total_tools_report_diff.py
@@ -8,27 +8,27 @@ uploaded_file_0 = st.file_uploader("Upload the prior month's file")
 uploaded_file_1 = st.file_uploader("Upload the current month's file")
 
 if uploaded_file_0 is not None and uploaded_file_1 is not None:
-    try:
-        prior_month = pd.read_excel(uploaded_file_0, engine = 'openpyxl', header = 1)
-        prior_month = prior_month.rename(columns={prior_month.columns[0]: 'Store'})
-        prior_month.iloc[0,0] = 'Summary'
-        prior_month_sliced = prior_month.iloc[:, :3]
-        
-        current_month = pd.read_excel(uploaded_file_1, engine = 'openpyxl', header = 1)
-        current_month = current_month.rename(columns={current_month.columns[0]: 'Store'})
-        current_month.iloc[0,0] = 'Summary'
-        merged_data = pd.merge(current_month, prior_month_sliced, on='Store', how='left')
+    #try:
+    prior_month = pd.read_excel(uploaded_file_0, engine = 'openpyxl', header = 1)
+    prior_month = prior_month.rename(columns={prior_month.columns[0]: 'Store'})
+    prior_month.iloc[0,0] = 'Summary'
+    prior_month_sliced = prior_month.iloc[:, :3]
+    
+    current_month = pd.read_excel(uploaded_file_1, engine = 'openpyxl', header = 1)
+    current_month = current_month.rename(columns={current_month.columns[0]: 'Store'})
+    current_month.iloc[0,0] = 'Summary'
+    merged_data = pd.merge(current_month, prior_month_sliced, on='Store', how='left')
 
-        merged_data['Monthly Sales'] = merged_data[:,-3] - merged_data[:,0]
-        merged_data['Monthly Profit'] = merged_data[:,-2] - merged_data[:,1]
-        merged_data['Monthly GP%'] = (merged_data['Monthly Profit'] / merged_data['Monthly Sales']).fillna(0)
+    merged_data['Monthly Sales'] = merged_data[:,-3] - merged_data[:,0]
+    merged_data['Monthly Profit'] = merged_data[:,-2] - merged_data[:,1]
+    merged_data['Monthly GP%'] = (merged_data['Monthly Profit'] / merged_data['Monthly Sales']).fillna(0)
 
-        st.dataframe(prior_month_sliced)
-        st.dataframe(current_month)
-        st.dataframe(merged_data)
+    st.dataframe(prior_month_sliced)
+    st.dataframe(current_month)
+    st.dataframe(merged_data)
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    #except Exception as e:
+        #st.error(f"An error occurred: {e}")
 
 else:
     st.info("Please upload a CSV file to get started.")
