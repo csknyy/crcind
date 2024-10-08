0cb49d2cfac5905d8c705afa429162a8bc8b70cd
csknyy <55134627+csknyy@users.noreply.github.com>
2024-10-09 11:36:09 +1300

    Update AU_Total_tools_report_diff.py

--- AU_Total_tools_report_diff.py
+++ AU_Total_tools_report_diff.py
@@ -9,8 +9,8 @@ uploaded_file_1 = st.file_uploader("Upload the current month's file")
 
 if uploaded_file_0 is not None and uploaded_file_1 is not None:
     try:
-        prior_month = pd.read_excel(uploaded_file_0, engine = 'openpyxl', sheet_name = 'Ranking',header = 1)
-        current_month = pd.read_excel(uploaded_file_1, engine = 'openpyxl', sheet_name = 'Ranking',header = 1)
+        prior_month = pd.read_excel(uploaded_file_0, engine = 'openpyxl', header = 1)
+        current_month = pd.read_excel(uploaded_file_1, engine = 'openpyxl', header = 1)
         
         st.dataframe(prior_month)
         st.dataframe(current_month)
