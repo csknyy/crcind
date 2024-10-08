bef071a2128af5eb1ceefb4394184ada276b99e2
csknyy <55134627+csknyy@users.noreply.github.com>
2024-10-09 11:35:50 +1300

    Update AU_Total_tools_report_diff.py

--- AU_Total_tools_report_diff.py
+++ AU_Total_tools_report_diff.py
@@ -9,14 +9,14 @@ uploaded_file_1 = st.file_uploader("Upload the current month's file")
 
 if uploaded_file_0 is not None and uploaded_file_1 is not None:
     try:
-      prior_month = pd.read_excel(uploaded_file_0, engine = 'openpyxl', sheet_name = 'Ranking',header = 1)
-      current_month = pd.read_excel(uploaded_file_1, engine = 'openpyxl', sheet_name = 'Ranking',header = 1)
+        prior_month = pd.read_excel(uploaded_file_0, engine = 'openpyxl', sheet_name = 'Ranking',header = 1)
+        current_month = pd.read_excel(uploaded_file_1, engine = 'openpyxl', sheet_name = 'Ranking',header = 1)
+        
+        st.dataframe(prior_month)
+        st.dataframe(current_month)
 
-      st.dataframe(prior_month)
-      st.dataframe(current_month)
-
-  except Exception as e:
-          st.error(f"An error occurred: {e}")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 else:
     st.info("Please upload a CSV file to get started.")
