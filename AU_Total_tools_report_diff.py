1a0f763db8a846a173b6e21dd726e41891eb962e
csknyy <55134627+csknyy@users.noreply.github.com>
2024-10-09 13:03:17 +1300

    Update AU_Total_tools_report_diff.py

--- AU_Total_tools_report_diff.py
+++ AU_Total_tools_report_diff.py
@@ -58,7 +58,8 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None:
     merged_data.iloc[-1, 2] = merged_data.iloc[:, 1].sum() / merged_data.iloc[:, 0].sum()
     
     merged_data['delete'] = np.where(merged_data.iloc[:, 3] != 0, merged_data.iloc[:, 3] / merged_data.iloc[:, 5], 0)
-    merged_data.iloc[-1, 5] = merged_data.iloc[:, 3].sum() / merged_data.iloc[:, -1].sum()
+    temp_total = merged_data.iloc[:-1, -1].sum()
+    merged_data.iloc[-1, 5] = merged_data.iloc[:, 3].sum() / temp_total
     
     
 
