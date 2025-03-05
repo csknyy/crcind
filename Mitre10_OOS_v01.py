a3694eb9ea59417c2a3fc7a5c122c4460bdff15c
csknyy <55134627+csknyy@users.noreply.github.com>
2025-03-05 13:25:50 +1300

    Update Mitre10_OOS_v01.py

--- Mitre10_OOS_v01.py
+++ Mitre10_OOS_v01.py
@@ -58,6 +58,23 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None:
         #csv = convert_data(data)
         #st.download_button(label="Download data as CSV", data=csv, file_name='Min_Max_with_supplier_request.csv', mime='text/csv')
 
+        st.markdown('---')
+
+        farm_source_article = ['204902','205186','205351','205876','210467','223155','262830','262831','262832','264292','265170','264285','264287','265747','265761','223157','205874','205875','250603','264932','223295']
+        farm_source_items = ['5040','3055','5035','2091','5070','5089','1751846','1751837','8498','1751839','1753211','EVR1','EVR5','1753427','1754785','8017','2087','2089','2125','1753336','5911']
+        farm_source_item_names = ['Lubricant Engine Start CRC 400mL','Lubricant 808 Silicone Spray CRC 500mL','Lubricant Tac 2 CRC 300g','Lubricant Prime It CRC 400mL','Degreaser Aeroclean 500ml','Degreaser Brakleen CRC 600g','Lubricant 556 CRC 4L','Lubricant Aerosol 556 CRC 420ml','Spray Seal Leak Stop 350gm','Lubricant 556 CRC Marine 420ml','Lubricant 808 Silicone 15% Extra 575ml','Lubricant Evapo-Rust 1L','Lubricant Evapo-Rust 5L','Cleaner Solar Panel WattsUp Conc 5L','Cleaner Solar Panel WattsUp Conc 1L','Adhesive Spray F2 Multipurpose 575mL','Paint Zinc Bright CRC 400mL','Paint Zinc Black CRC 400mL','Spray CRC Zinc It 500gm','Spray Evapo-Rust Gel CRC 500g','Fungicide Clene Up Slow Release CRC 5L']
+
+        farm_source_map = pd.DataFrame()
+        farm_source_map['Article Vendor'] = farm_source_article
+        farm_source_map['Vendor Article Number'] = farm_source_items
+        farm_source_map['Description'] = farm_source_item_names
+        
+        farm_source_data = data_BSS[data_BSS['Legacy'].isin(farm_source_items)]
+
+        merged_data = pd.merge(farm_source_map, farm_source_data, left_on='Vendor Article Number', right_on='Legacy', how='inner')
+
+        st.dataframe(merged_data)
+    
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
