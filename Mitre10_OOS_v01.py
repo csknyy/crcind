72a0c6ddbf774bcc117d0aac950dcc1e9124fdab
csknyy <55134627+csknyy@users.noreply.github.com>
2025-07-21 13:09:59 +1200

    Update Mitre10_OOS_v01.py

--- Mitre10_OOS_v01.py
+++ Mitre10_OOS_v01.py
@@ -21,11 +21,9 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None and uploaded_file
         data_available_physical = pd.read_excel(uploaded_file_2, engine = 'openpyxl')
         
         #####################################
-
-        st.dataframe(data_available_physical)
         
         data = pd.merge(data_BSS['Legacy'], data_M10_ranking[['Supplier Item Code', 'M10 Code','Item', 'Department', 'Range']], how='left', left_on='Legacy', right_on='Supplier Item Code')
-        data = pd.merge(data, data_available_physical[['Search name','Available physical']],how='left', left_on='Legacy')
+        data = pd.merge(data, data_available_physical[['Search name','Available physical']],how='left', left_on='Legacy', right_on='Search name')
         
         data['SOH Status'] = ''
         
