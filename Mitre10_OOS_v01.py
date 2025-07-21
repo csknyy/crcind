094a246abe3a6a3452f93887f9940b42fcd75469
csknyy <55134627+csknyy@users.noreply.github.com>
2025-07-21 13:36:29 +1200

    Update Mitre10_OOS_v01.py

--- Mitre10_OOS_v01.py
+++ Mitre10_OOS_v01.py
@@ -54,7 +54,7 @@ if uploaded_file_0 is not None and uploaded_file_1 is not None and uploaded_file
         
         #data = data[(data['Physical inventory']==0) & (data['M10 Code'] != 0)]
         #Working# data = data[(data['Available physical']==0) & (data['M10 Code'] != 0)]
-        data = data[data['Available physical']==0]
+        data = data[(data['Available physical']==0) & (data['M10 Code'] != 0)]
 
         #remove_cols = ['Physical inventory',' ','Available physical','Search name']
         #for col in remove_cols:
