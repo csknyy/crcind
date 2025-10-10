f29dcfd83a7f97c54120dfcb19857432a7a91953
csknyy <55134627+csknyy@users.noreply.github.com>
2025-10-10 14:38:30 +1300

    Update scrape_CRC_NZ.py

--- scrape_CRC_NZ.py
+++ scrape_CRC_NZ.py
@@ -23,6 +23,8 @@ if len(crc_codes)>0:
     for i in crc_codes_list:
       try:
         url = url_base + 'catalogsearch/result/?q=' + str(i)
+
+        print(url)
   
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
